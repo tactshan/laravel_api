ab983d3478bb3755a1b4514ea6920a5bacd24d2d
tactshan <329835044@qq.com>
2019-03-14 10:39:33 +0000

    first commit

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+class UserController
+{
+    public $redis_h_u_key = 'h:user_token_u:';
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+    public function test()
+    {
+        echo __METHOD__;
+    }
+    //用户登录
+    public function userLogin(Request $request)
+    {
+       $user_name = $request->input('u');
+       $pass = $request->input('p');
+       $res=true;
+       //登陆成功验证用户信息
+       if($res){
+           $uid = 1000;
+           $str = time().$uid.mt_rand(1111,9999);
+           $token=substr(md5($str),10,20);
+
+           //保存到redis中
+           $key = $this->redis_h_u_key.$uid;
+           Redis::hSet($key,'token',$token);
+           Redis::expire($key,3600*24*7);
+       }else{
+           // TODO 登录失败
+       }
+    }
+
+    //识别用户信息
+    public function vip()
+    {
+        var_dump($http_response_header);
+    }
+}
