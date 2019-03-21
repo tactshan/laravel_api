cceaf144f701318a29470cd37cd129e03f078c45
tactshan <329835044@qq.com>
2019-03-21 08:27:00 +0000

    debug

--- app/Http/Middleware/CheckLoginMiddleware.php
+++ app/Http/Middleware/CheckLoginMiddleware.php
@@ -9,10 +9,13 @@ class CheckLoginMiddleware
 {
     public function handle($request, Closure $next)
     {
+//        setcookie('uid','1',time()+86400,'/','',false,true);
+//        setcookie('token','6ad38950977cd24a1ed6',time()+86400,'/','',false,true);
+//        die;
        if(isset($_COOKIE['token']) && isset($_COOKIE['uid'])){
            //验证token
            $key = 'h:user_token_u:'.$_COOKIE['uid'];
-           $token = Redis::hgetall($key);
+           $token = Redis::hget($key,'token');
            if($_COOKIE['token']!==$token){
                //有效
                $request->attributes->add(['is_login'=>1]);
